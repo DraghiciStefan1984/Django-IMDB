bb08611fbd1648da8432a146172d208ecc4c0968
draghicistefan <draghicistefan1984@gmail.com>
2020-04-15 18:38:47 +0300

    added gategory and language filters

--- imdb/imdb/urls.py
+++ imdb/imdb/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('movie.urls')),
+    path('', include('movie.urls', namespace='movies')),
 ]
 urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
