288be94ab0e1e6ea06944de93a9a047bd6fa8841
DraghiciStefan1984 <draghicistefan1984@gmail.com>
2020-04-16 10:07:52 +0300

    related movies start

--- imdb/movie/models.py
+++ imdb/movie/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.utils.text import slugify
+from django.utils import timezone
 
 # Create your models here.
 CATEGORY_CHOICES=(
@@ -31,6 +32,14 @@ class Movie(models.Model):
     year_of_production=models.DateField()
     views_count=models.IntegerField(default=0)
     cast=models.CharField(max_length=1000)
+    slug=models.SlugField(blank=True, null=True)
+    trailer=models.URLField()
+    created=models.DateTimeField(default=timezone.now())
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug=slugify(self.title)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
--- imdb/movie/views.py
+++ imdb/movie/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.dates import YearArchiveView
 from .models import Movie, MovieLink
 
 # Create your views here.
@@ -21,6 +22,7 @@ class MovieDetail(DetailView):
     def get_context_data(self, **kwargs):
         context=super().get_context_data(**kwargs)
         context['links']=MovieLink.objects.filter(movie=self.get_object())
+        context['realted_movies']=Movie.objects.filter(category=self.get_object().category).order_by('created')[:3]
         return context
 
 
@@ -50,3 +52,24 @@ class MovieLanguage(ListView):
         context=super().get_context_data(**kwargs)
         context['movie_language']=self.language
         return context
+
+
+class MovieSearch(ListView):
+    model=Movie
+    paginate_by=1
+
+    def get_queryset(self):
+        query=self.request.GET.get('query')
+        if query:
+            object_list=self.model.objects.filter(title__icontains=query)
+        else:
+            object_list=self.model.objects.none()
+        return object_list
+
+
+class MovieYear(YearArchiveView):
+    queryset=Movie.objects.all()
+    date_field='year_of_production'
+    make_object_list=True
+    allow_future=True
+    print(queryset)
